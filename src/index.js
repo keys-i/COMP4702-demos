373af9973cbcc9a4d413e71c895d06613ef30951
Keys <70819367+keys-i@users.noreply.github.com>
2025-04-10 11:23:52 +1000

    Added package.json touches for hosting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -9,7 +9,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter basename="/your-repo-name">
-    <App />
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root"),
 );
